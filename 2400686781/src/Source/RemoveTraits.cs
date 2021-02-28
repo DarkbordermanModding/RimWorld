5cdd038f80984f22dd1feebc7e930d9db1083bf5
Darkborderman <reastw1234@gmail.com>
2021-02-28 15:10:19 +0800

    refactor: Update mods

--- 2400686781/src/Source/RemoveTraits.cs
+++ 2400686781/src/Source/RemoveTraits.cs
@@ -2,18 +2,68 @@
 using System.Linq;
 using Verse;
 using RimWorld;
+using UnityEngine;
 
 namespace RemoveTraits
 {
+    public class RemoveTraitsSettings : ModSettings
+    {
+        public bool notifyWhenSuccess = true;
+        public Dictionary<string, bool> disableDict = new Dictionary<string, bool>();
+        public bool displayBool = false;
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Collections.Look(ref disableDict, "disableDict");
+        }
+    }
+    class RemoveTraitsMod : Mod
+    {
+        //private bool recheckDefList = true;
+        public static Vector2 scrollPosition = new Vector2();
+        public static RemoveTraitsSettings settings;
+        public RemoveTraitsMod(ModContentPack content) : base(content)
+        {
+            settings = GetSettings<RemoveTraitsSettings>();
+        }
+        public override string SettingsCategory() => "Remove Traits";
+        public override void DoSettingsWindowContents(Rect inRect) {
+
+            Listing_Standard ls = new Listing_Standard();
+            Rect rect2 = new Rect(0f, 0f, inRect.width/2 - 16f, inRect.height + DefDatabase<TraitDef>.AllDefs.Count() * 25);
+            Widgets.BeginScrollView(inRect, ref scrollPosition, rect2, true);
+            ls.Begin(rect2);
+
+            ls.CheckboxLabeled("Notify when surgery succeed", ref settings.notifyWhenSuccess);
+            ls.Label("Keep Trait List", -1, "Check the trait to prevent being removed in midgame.");
+
+            foreach (TraitDef def in DefDatabase<TraitDef>.AllDefs.OrderBy(item => item.defName).ToList())
+            {
+                bool index = settings.disableDict.TryGetValue(def.ToString(), false);
+                ls.CheckboxLabeled(def.ToString(), ref index);
+                settings.disableDict[def.ToString()] = index;
+            }
+            settings.Write();
+            ls.End();
+            Widgets.EndScrollView();
+        }
+
+    }
+
     public class RemoveTraits : Recipe_Surgery
     {
         public override bool AvailableOnNow(Thing thing)
         {
+
             if (thing is Pawn pawn)
             {
-                if (pawn.story.traits.allTraits.Count != 0)
+                foreach(Trait trait in pawn.story.traits.allTraits)
                 {
-                    return true;
+                    //Not in disabled trait mean can be surgeryed.
+                    if (!RemoveTraitsMod.settings.disableDict.TryGetValue(trait.def.defName, false))
+                    {
+                        return true;
+                    }
                 }
             }
             return false;
@@ -26,25 +76,37 @@ namespace RemoveTraits
                 return;
             }
 
-            int index = new IntRange(0, pawn.story.traits.allTraits.Count - 1).RandomInRange;
-            Log.Message("Try to remove " + pawn.story.traits.allTraits.ElementAt(index).def.defName + " from pawn " + pawn.NameShortColored);
-            pawn.story.traits.allTraits.RemoveAt(index);
+            int index = 0;
+            foreach(Trait trait in pawn.story.traits.allTraits)
+            {
+                //Not in disabled trait mean can be surgeryed.
+                if (!RemoveTraitsMod.settings.disableDict.TryGetValue(trait.def.defName, false))
+                {
+                    pawn.story.traits.allTraits.RemoveAt(index);
+                    // Update pawn status
+                    if (!pawn.Dead && pawn.RaceProps.Humanlike)
+                    {
+                        pawn.needs.mood.thoughts.situational.Notify_SituationalThoughtsDirty();
+                    }
+                    if (pawn.skills != null)
+                    {
+                        pawn.skills.Notify_SkillDisablesChanged();
+                    }
+                    if (pawn.workSettings != null)
+                    {
+                        pawn.workSettings.Notify_DisabledWorkTypesChanged();
+                        pawn.workSettings.Notify_UseWorkPrioritiesChanged();
+                    }
 
-            // Update pawn status
-            if (!pawn.Dead && pawn.RaceProps.Humanlike)
-            {
-                pawn.needs.mood.thoughts.situational.Notify_SituationalThoughtsDirty();
+                    // Notify
+                    if (RemoveTraitsMod.settings.notifyWhenSuccess){
+                        Messages.Message("Successfully remove " + pawn.ToString() + "'s trait. (" + trait.def.defName + ")", pawn, MessageTypeDefOf.PositiveEvent);
+                    }
+                    Log.Message("Removed " + trait.def.defName + " from pawn " + pawn.NameShortColored);
+                    break;
+                }
+                index++;
             }
-            if (pawn.skills != null)
-            {
-                pawn.skills.Notify_SkillDisablesChanged();
-            }
-            if (pawn.workSettings != null)
-            {
-                pawn.workSettings.Notify_DisabledWorkTypesChanged();
-                pawn.workSettings.Notify_UseWorkPrioritiesChanged();
-            }
-            Messages.Message("Successfully remove " + pawn.ToString() + "'s trait.", pawn, MessageTypeDefOf.PositiveEvent);
             return;
         }
     }
